201070f83fcac39a675a93f17b5b28cd6d4af7bb
Sasha Stepanova <chernishevskie@yandex.ru>
2021-09-09 17:04:57 -0300

    feat: added code for openning  and closing the projects conteiners

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,7 +1,14 @@
-const sections = document.querySelectorAll('section'); // селекторы
-const underlineLink = document.querySelector('.link__underline'); 
+const sections = document.querySelectorAll('section'); // sections
+const underlineLink = document.querySelector('.link__underline'); // under line element
+const russianTravel = document.querySelector('#russianTravel'); // container for RT project
+const mesto = document.querySelector('#mesto'); //container for mesto project
+const blog = document.querySelector('#blog'); //container for blog project
+const openRtContainer = document.querySelector('#openRtContainer'); //button that open RT container
+const openMestoContainer = document.querySelector('#openMestoContainer'); //button that open mesto container
+const openBlogContainer = document.querySelector('#openBlogContainer'); //button that open blog container
+const steckContainers = document.querySelectorAll('.projects__item_description'); //all containers
 const options = {
-    threshold: 0.4 // промежуток на котором срабатывает отображение секции
+    threshold: 0.4
 };
 
 let observe = new IntersectionObserver(navCheck, options);
@@ -27,3 +34,38 @@ function navCheck(entries) {
 sections.forEach( section => {
     observe.observe(section);
 });
+
+const openContiner = (container) => { //function of oppenin the section
+    container.classList.remove('unactive');
+    container.classList.add('active');
+    document.addEventListener('keydown', closeByEsc);
+}
+ 
+const closeContainer = (container) => { //function of closing the section
+    container.classList.remove('active'); 
+    container.classList.add('unactive');
+    document.removeEventListener('keydown', closeByEsc); 
+} 
+
+const closeByEsc = (event) => { //function of closing the section by click at ESC
+    const deleteClass = document.querySelector('.active');  
+    if(event.key === "Escape") { 
+        closeContainer(deleteClass); 
+    }    
+}
+
+steckContainers.forEach((container) => { //function of closing the section by clicking the button close or  overlay 
+    container.addEventListener('click', (evt) => { 
+        if(evt.target.classList.contains('active')) { 
+            closeContainer(container);
+        } 
+        if(evt.target.classList.contains('cancel-cross')) { 
+            closeContainer(container);
+        } 
+    }) 
+}) 
+
+openRtContainer.addEventListener('click', () => openContiner(russianTravel));
+openMestoContainer.addEventListener('click', () => openContiner(mesto));
+openBlogContainer.addEventListener('click', () => openContiner(blog));
+
